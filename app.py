32ac0a2dcbe669dd2e433c1ec1146e6fe00694c9
Alessandro Barbaro <barbaro222alessandro@gmail.com>
2023-04-14 16:08:42 +0200

    aaa

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+
 import streamlit as st
+import pandas as pd
+
+st.title("Area Triang")
+
+st.image("https://raw.githubusercontent.com/Frenz86/IFTS23/main/streamlit/bb.png", c)
+
+def areatriangolo(l1:float,l2:float):
+    a = (l1*l2)/2
+    return a 
+
+def main():
+    st.text("Area triangoloooooo")
+    # slider
+    num1 = st.slider('Please inserisci base', 0, 100, 25)
+    num2 = st.slider('Please inserisci altezza', 0, 100, 35)
+    r = areatriangolo(num1,num1)
+
+    st.write("l'area del triangolo è")
+
+
+
+if __name__ == "__main__":
+    main()
